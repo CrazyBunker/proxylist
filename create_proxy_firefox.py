6c2db6d0e68657ea8cd5d4af82fd2940c15d0638
Владислав Плоткин <vlad@krasnodar-it-service.ru>
2019-06-23 15:21:18 +0300

    Created verbose out

--- create_proxy_firefox.py
+++ create_proxy_firefox.py
@@ -1,29 +1,30 @@
 from proxylist import proxylist
-import json
+from cacheJson import cacheJson
 from jinja2 import Template
-proxy = proxylist()
+
 isDone = True
 rewrite = False
-#test = {'domain1': {'ip': 1123123, 'port': 2341}}
-with open('.cache.json') as cache:
-     proxyForDomain = json.load(cache)
-for domain in proxyForDomain:
-     if not proxy.testProxy(domain, proxyForDomain[domain]['ip'], proxyForDomain[domain]['port']):
-         proxy.set_type(['HTTPS'])
-         proxy.excCountry(['RU'])
-         answer = proxy.verify(domain)
-         proxyForDomain[domain] = answer
-     else:
-         isDone = False
-         print('Saved proxy is done')
+
+proxy = proxylist()
+cache = cacheJson('.cache.json')
+proxy.verbose = 5
+for domain in cache.json:
+      cache.domain = domain
+      proxy.url = domain
+      if not proxy.testProxy(cache.data()):
+          proxy.set_type(['HTTPS'])
+          proxy.excCountry(['RU'])
+          answer = proxy.verify()
+          cache.writeItem(answer)
+      else:
+          isDone = False
 if isDone or rewrite:
-    with open('.cache.json','w') as writeCache:
-        json.dump(proxyForDomain,writeCache)
-    html = open('proxy.pac.j2').read()
-    template = Template(html)
-    pacFile = template.render(block=proxyForDomain)
-    with open('/home/qq/.proxy.pac','w') as pac:
-        pac.write(pacFile)
+     cache.saveToCacheFile()
+     html = open('proxy.pac.j2').read()
+     template = Template(html)
+     pacFile = template.render(block=cache.json)
+     with open('/home/qq/.proxy.pac','w') as pac:
+         pac.write(pacFile)
 
 
 
