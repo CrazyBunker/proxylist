30e20ee60dbadf422870628efc4ba6361d140713
Владислав Плоткин <vlad@krasnodar-it-service.ru>
2019-06-23 18:28:17 +0300

    Added parser parameter

--- create_proxy_firefox.py
+++ create_proxy_firefox.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
-# # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from proxylist import proxylist
 from cacheJson import cacheJson
 from jinja2 import Template
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Search proxy server and update pac file for firefox.')
+parser.add_argument('-c', '--config', default=".cache.json", help="Path to config file in format json")
+parser.add_argument('-p', '--pac', default=".proxy.pac", help="Path to output pac file for firefox")
+parser.add_argument('-t', '--template', default="proxy.pac.j2", help="Path to templete for pac file")
+parser.add_argument('-v', '--verbose', default=0, type=int, help="Verbose output")
+args = parser.parse_args()
+
 isDone = True
 rewrite = False
 proxy = proxylist()
-cache = cacheJson('.cache.json')
+proxy.verbose = args.verbose
+if proxy.verbose > 5:
+    proxy.verbose = 5
+cache = cacheJson(args.config)
 for domain in cache.json:
       cache.domain = domain
       proxy.url = domain
@@ -19,10 +32,10 @@ for domain in cache.json:
           isDone = False
 if isDone or rewrite:
      cache.saveToCacheFile()
-     html = open('proxy.pac.j2').read()
+     html = open(args.template).read()
      template = Template(html)
      pacFile = template.render(block=cache.json)
-     with open('/home/qq/.proxy.pac','w') as pac:
+     with open(args.pac,'w') as pac:
          pac.write(pacFile)
 
 
